15139888f7fe6cd85fa24667f10283525e095922
devalc <deva4998@vandals.uidaho.edu>
2019-04-29 16:45:17 -0700

    updated nlm Langmuir and created nlm func for Freundlich

--- R/NonLinearFreundlichPlot.R
+++ R/NonLinearFreundlichPlot.R
@@ -4,20 +4,20 @@
 #' @param df data frame containing the data
 #' @param Ce equilibrium solution concentrations in mg/l
 #' @param Qe retention by solid (adsorption) in mg/kg
-#' @param Qmax_guess Guestimate of Qmax
-#' @param Kl_guess Guestimate of Kl
+#' @param n_guess Guestimate of n
+#' @param Kf_guess Guestimate of Kf
 #' @return A plot of predicted non linear form of Langmuire model
 #' with the Qmax (maximum sorption capacity) and Kl (langmuir constant
 #' pertaining to the affinity of substrate towards the chemical) values
 #' @export
 
 
-NonLinearLangmuirPlot <- function(df, Ce, Qe, Qmax_guess, Kl_guess){
-    nlm_model<- nls(formula = Qe ~ (Qmax*kl*Ce/(1+kl*Ce)),  data = df, start = list(Qmax = Qmax_guess, kl = Kl_guess), algorith = "port" )
+NonLinearFreundlichPlot <- function(df, Ce, Qe, n_guess, Kf_guess){
+    nlm_model<-nls(formula = Qe ~ kf*(Ce)**n,  data = df, start = list(n = n_guess, kf = Kf_guess), algorith = "port" )
     q <- summary(nlm_model)
     coef<-round(coef(nlm_model), 2)
-    Qmax<- paste0("Qmax (mg/kg) = ", coef[1])
-    Kl<-  paste0("Kl (L/kg)= ", coef[2] )
+    Qmax<- paste0("n = ", coef[1])
+    Kl<-  paste0("Kf (L/kg)= ", coef[2] )
     xNew <- seq(range(Ce)[1],range(Ce)[2],length.out = 200)
     yNew <- predict(nlm_model,list(Ce = xNew))
     plot(Ce, Qe,  pch=25, bg = "blue", xlab = "Ce (mg/L)",
